f36656928f8e69d0e11c7aca17af293fe7ca0809
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-09-22 17:36:46 +0200

    @Slf4j does not work

--- src/main/java/da/project/sporteezone/app/controller/LekceController.java
+++ src/main/java/da/project/sporteezone/app/controller/LekceController.java
@@ -4,6 +4,13 @@ import da.project.sporteezone.app.entity.Fitness;
 import da.project.sporteezone.app.entity.Lekce;
 import da.project.sporteezone.app.repository.FitnessRepository;
 import da.project.sporteezone.app.repository.LekceRepository;
+import lombok.extern.java.Log;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import lombok.extern.slf4j.Slf4j;
+import org.apache.logging.slf4j.Log4jLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.*;
@@ -17,6 +24,7 @@ import java.util.List;
 import java.util.Optional;
 
 
+@Slf4j
 @RestController
 @RequestMapping(path = "/api/v1/lekce")
 public class LekceController {
@@ -30,6 +38,7 @@ public class LekceController {
         return lekceRepository.findAll();
     }
 
+
     @GetMapping(path = "/byDatum")
     public @ResponseBody
     List<Lekce> findLekceByDateTime(
@@ -39,22 +48,14 @@ public class LekceController {
         @RequestParam("konec")
         @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime konec) {
 
+
         System.out.println("datum je " + zacatek);
         System.out.println(zacatek.getClass());
         return lekceRepository.findAllByZacatekBetween(zacatek, konec);
     }
 
 
-
-    //public void dateTime(@RequestParam("localDateTime")
-    //                     @DateTimeFormat(iso = DateTimeFormat.ISO.DATE_TIME) LocalDateTime localDateTime) {
-        // ...
-
-
-
-
     // API call - GET  localhost:8090/api/v1/lekce/byDatum?zacatek=2020-09-13%2015:00&konec=2020-09-13%2017:00
-
 
 }
 
