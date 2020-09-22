8abf1690802ed4380d363a2e3e940bdb03b18d56
Vlastimil Dolejš <vlastimil.dolejs@diribet.cz>
2020-09-22 08:58:51 +0200

    added lombok

--- src/main/java/da/project/sporteezone/app/entity/Fitness.java
+++ src/main/java/da/project/sporteezone/app/entity/Fitness.java
@@ -1,27 +1,13 @@
 package da.project.sporteezone.app.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "fitness")
+@Data
 public class Fitness {
-    @Override
-    public String toString() {
-        return "Fitness{" +
-            "id=" + id +
-            ", nazev='" + nazev + '\'' +
-            ", url='" + url + '\'' +
-            ", ulice='" + ulice + '\'' +
-            ", mesto='" + mesto + '\'' +
-            ", psc='" + psc + '\'' +
-            ", multisport=" + multisport +
-            ", activepass=" + activepass +
-            ", platba='" + platba + '\'' +
-            ", pohlavi='" + pohlavi + '\'' +
-            ", mhd=" + mhd +
-            '}';
-    }
 
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -38,131 +24,4 @@ public class Fitness {
     private String pohlavi;
     private Boolean mhd;
 
-
-    public Fitness() {
-    }
-
-
-    public Fitness(String nazev, String url, String ulice, String mesto, String psc, Boolean multisport, Boolean activepass, String platba, String pohlavi
-        , Boolean mhd) {
-        this.nazev = nazev;
-        this.ulice = ulice;
-        this.url = url;
-        this.mesto = mesto;
-        this.psc = psc;
-        this.multisport = multisport;
-        this.activepass = activepass;
-        this.platba = Fitness.this.platba;
-        this.pohlavi
-            = pohlavi
-        ;
-        this.mhd = mhd;
-    }
-
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public Fitness(Integer id, String url, String nazev, String ulice, String mesto, String psc, Boolean multisport, Boolean activepass, String platba, String pohlavi
-        , Boolean mhd) {
-        this.id = id;
-        this.nazev = nazev;
-        this.url= url;
-        this.ulice = ulice;
-        this.mesto = mesto;
-        this.psc = psc;
-        this.multisport = multisport;
-        this.activepass = activepass;
-        this.platba = Fitness.this.platba;
-        this.pohlavi
-            = pohlavi
-        ;
-        this.mhd = mhd;
-    }
-
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setNazev(String nazev) {
-        this.nazev = nazev;
-    }
-
-    public void setUlice(String ulice) {
-        this.ulice = ulice;
-    }
-
-    public void setMesto(String mesto) {
-        this.mesto = mesto;
-    }
-
-    public void setPsc(String psc) {
-        this.psc = psc;
-    }
-
-    public void setMultisport(Boolean multisport) {
-        this.multisport = multisport;
-    }
-
-    public void setActivepass(Boolean activepass) {
-        this.activepass = activepass;
-    }
-
-    public void setPlatba(String platba) {
-        this.platba = platba;
-    }
-
-    public void setPohlavi(String pohlavi) {
-        this.pohlavi = pohlavi;
-    }
-
-    public void setMhd(Boolean mhd) {
-        this.mhd = mhd;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNazev() {
-        return nazev;
-    }
-
-    public String getUlice() {
-        return ulice;
-    }
-
-    public String getMesto() {
-        return mesto;
-    }
-
-    public String getPsc() {
-        return psc;
-    }
-
-    public Boolean getMultisport() {
-        return multisport;
-    }
-
-    public Boolean getActivepass() {
-        return activepass;
-    }
-
-    public String getPlatba() {
-        return platba;
-    }
-
-    public String getPohlavi() {
-        return pohlavi;
-    }
-
-    public Boolean getMhd() {
-        return mhd;
-    }
 }
--- src/main/java/da/project/sporteezone/app/entity/Lekce.java
+++ src/main/java/da/project/sporteezone/app/entity/Lekce.java
@@ -1,12 +1,14 @@
 package da.project.sporteezone.app.entity;
 
+import lombok.Data;
+
 import javax.persistence.*;
-import java.sql.Time;
 import java.util.Date;
 
 
 @Entity
 @Table(name = "lekce")
+@Data
 public class Lekce {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
@@ -27,116 +29,5 @@ public class Lekce {
     private Integer kodFitko;
     private Integer kodLektor;
 
-    public Lekce() {
-    }
-
-    @Override
-    public String toString() {
-        return "Lekce{" +
-            "id=" + id +
-            ", nazev='" + nazev + '\'' +
-            ", url='" + url + '\'' +
-            ", zacatek=" + zacatek +
-            ", konec  =" + konec +
-            ", kapacita=" + kapacita +
-            ", nutnotRezervace=" + nutnostRezervace +
-            ", cena=" + cena +
-            ", kodFitko=" + kodFitko +
-            ", kodLektor=" + kodLektor +
-            '}';
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public void setNazev(String nazev) {
-        this.nazev = nazev;
-    }
-
-    public void setUrl(String url) {
-        this.url = url;
-    }
-
-    public void setZacatek(Date zacatek) {
-        this.zacatek = zacatek;
-    }
-
-    public void setKonec(Date konec) {
-        this.konec = konec;
-    }
-
-    public void setKapacita(Integer kapacita) {
-        this.kapacita = kapacita;
-    }
-
-    public void setNutnostRezervace(Boolean nutnostRezervace) {
-        this.nutnostRezervace = nutnostRezervace;
-    }
-
-    public void setCena(Integer cena) {
-        this.cena = cena;
-    }
-
-    public void setKodFitko(Integer kodFitko) {
-        this.kodFitko = kodFitko;
-    }
-
-    public void setKodLektor(Integer kodLektor) {
-        this.kodLektor = kodLektor;
-    }
-
-    public Integer getId() {
-        return id;
-    }
-
-    public String getNazev() {
-        return nazev;
-    }
-
-    public String getUrl() {
-        return url;
-    }
-
-    public Date getZacatek() {
-        return zacatek;
-    }
-
-    public Date getTimeEnd() {
-        return konec;
-    }
-
-    public Integer getKapacita() {
-        return kapacita;
-    }
-
-    public Boolean getNutnostRezervace() {
-        return nutnostRezervace;
-    }
-
-    public Integer getCena() {
-        return cena;
-    }
-
-    public Integer getKodFitko() {
-        return kodFitko;
-    }
-
-    public Integer getKodLektor() {
-        return kodLektor;
-    }
-
-    public Lekce(Integer id, String nazev, String url, Date zacatek, Date konec, Integer kapacita, Boolean nutnostRezervace, Integer cena, Integer kodFitko, Integer kodLektor) {
-        this.id = id;
-        this.nazev = nazev;
-        this.url = url;
-        this.zacatek = zacatek;
-        this.konec = konec;
-        this.kapacita = kapacita;
-        this.nutnostRezervace = nutnostRezervace;
-        this.cena = cena;
-        this.kodFitko = kodFitko;
-        this.kodLektor = kodLektor;
-    }
 }
 
