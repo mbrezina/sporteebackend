d341e21c1169607477d9f81cc229a6f752fb57ac
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-09-26 21:47:21 +0200

    user end point needs a user with admin role

---
+++ src/main/java/da/project/sporteezone/app/service/UserPrincipalDetailService.java
@@ -0,0 +1,9 @@
+package da.project.sporteezone.app.service;
+
+import org.springframework.security.core.userdetails.UserDetailsService;
+
+//public class UserPrincipalDetailService implements UserDetailsService {
+//return null;
+
+//}
+
