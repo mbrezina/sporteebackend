dfea1f57ae4378bb30f6c7e649fd90862b44fed0
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-09-27 12:01:35 +0200

    make authentication with users in the DB work

--- src/main/java/da/project/sporteezone/app/service/UserPrincipalDetailService.java
+++
@@ -1,9 +0,0 @@
-package da.project.sporteezone.app.service;
-
-import org.springframework.security.core.userdetails.UserDetailsService;
-
-//public class UserPrincipalDetailService implements UserDetailsService {
-//return null;
-
-//}
-
