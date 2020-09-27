6153f0de66ea46a0bd0b2172f94f57908083cebd
Martina Burjanova <martina.burjanova@konicaminolta.cz>
2020-09-27 17:40:23 +0200

    attempt to handle exception

--- src/main/java/da/project/sporteezone/app/service/UserPrincipalDetailsService.java
+++ src/main/java/da/project/sporteezone/app/service/UserPrincipalDetailsService.java
@@ -4,12 +4,15 @@ import da.project.sporteezone.app.UserPrincipal;
 import da.project.sporteezone.app.entity.User;
 import da.project.sporteezone.app.repository.UserRepository;
 import javassist.bytecode.stackmap.BasicBlock;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
+
+@Slf4j
 @Service
 public class UserPrincipalDetailsService implements UserDetailsService {
 
@@ -19,9 +22,16 @@ public class UserPrincipalDetailsService implements UserDetailsService {
     //loads a user from our database and converts it to UserPrincipal
     @Override
     public UserDetails loadUserByUsername(String s) throws UsernameNotFoundException {
-        User user = this.userRepository.findByUsername(s);
-        UserPrincipal userPrincipal = new UserPrincipal(user);
-        return userPrincipal;
+
+        try {
+            User user = this.userRepository.findByUsername(s);
+            UserPrincipal userPrincipal = new UserPrincipal();
+            return userPrincipal;
+        }
+        catch(UsernameNotFoundException e) {
+            log.info("Username does not exist in the database: " + e);
+        }
+        return null;
     }
 
 }
